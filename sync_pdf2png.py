a209ea9dce8a4ad32edabfbac08b801c133575eb
wesky93 <wesky93@gmail.com>
2016-05-26 02:55:36 +0900

    pdf2png 초안
    현재 wand에서 pdf.format사용시 " png"가 지원하지 않는 다는 오류가 있음

---
+++ sync_pdf2png.py
@@ -0,0 +1,41 @@
+import os
+import datetime
+import glob
+from wand.image import Image
+
+
+def ctime_prefix(name):
+    """
+    파일 생성 시간을 취득하여 prefix를 생성해주는 함수
+    :param path: 파일 절대경로
+    :return: 파일 생성시간 prefix (str)
+    """
+    ctime = os.path.getmtime(name)  # 파일 생성시간 취득
+    ctime = "/" + str(datetime.datetime.fromtimestamp(ctime))[:10] + "_"
+    return ctime
+
+
+sync = "/Users/sinsky/Documents/Sync"  # sync의 파일이 자동 동기화 되는 폴더 절대 경로
+
+pdflist = glob.glob("%s/*.pdf" % (sync))  # snyc의 폴더에서 pdf파일만 추출
+
+for pdf in pdflist:
+    rename = "{syncfolder}{prefix}{filename}".format(syncfolder=sync, prefix=ctime_prefix(pdf),
+                                                     filename=pdf[len(sync) + 1:])
+    os.rename(pdf, rename)
+
+    # pdf를 png로 변환
+    with Image(filename=rename, resolution=300) as pdf:
+        rename = rename[:-3] + "png"
+        pdf.format = "png"
+        pdf.save(filename=rename)
+        print(rename)
+"""
+오류 발생
+Traceback (most recent call last):
+  File "/Users/sinsky/code/python/sync2png/sync_pdf2png.py", line 30, in <module>
+    pdf.format = "png"
+  File "/Users/sinsky/code/vertualenv/pdf2png/lib/python3.5/site-packages/wand/image.py", line 2711, in format
+    raise ValueError(repr(fmt) + ' is unsupported format')
+ValueError: 'png' is unsupported format
+"""
