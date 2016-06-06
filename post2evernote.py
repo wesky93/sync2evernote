3a672847fa1f5f5ca16385955170b30adf9d5919
wesky93 <wesky93@gmail.com>
2016-06-06 03:56:32 +0000

    make create notebook def

--- post2evernote.py
+++ post2evernote.py
@@ -7,14 +7,19 @@ import evernote.edam.type.ttypes as Types
 from os import listdir
 from evernote.api.client import EvernoteClient
 
-에버노트에 노트북 생성해주는 함수
-def mk_notebook(nbooks,note_store):
+# 에버노트에 노트북 생성해주는 함수
+# 할일 : 코드 테스트
+def mk_notebook(nbooks,note_store,workbook):
 	for nbook in nbooks:
-		
+		notebook = Types.Notebook()
+		notebook.name = nbook
+		cre_notebook = note_store.createNotebook(notebook)
+		workbook[nbook] = cre_notebook.GUID
+	return workbook 
 
 
 
-# 노트북 조회 함수
+# 노트북 조회후 에버노트에 없는 노트북은 생성하여 노트북을 키값으로 하는 딕셔너리 반환
 def match_notebooks(work_path,client=client,):
 	note_store = client.get_note_store()
 	notebooks = note_store.listNotebooks()	#에버노트 노트북 목록 조회
@@ -31,9 +36,12 @@ def match_notebooks(work_path,client=client,):
 			nmatch_books.append(nbook)	# 에버노트에 없는 노트북은 리스트에 추가
 	# 할일 : nmatch_books의 노트북을 에버노트에 생성하는 함수 제작,이후 노트북 딕셔너리에 추가
 	# 할일 : workbook의 노트북 guid를 구해 값으로 할당하는 함수 제작
+	
 	return workbook
 	
-			
+# 노트북의 guid를 찾아 딕셔너리 value값으로 넣어 반환
+def find_guid():
+	pass			
 	
 		
 				
