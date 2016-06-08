c8ef5e7a79a224980a90660db914ab75a5a0be9c
wesky93 <wesky93@gmail.com>
2016-06-08 20:36:10 +0900

    에버노트 업로드시 생성날짜를 추가 하기 위한 작업
    기존에 "에포치"시간을 가독성있는 시간으로 바꾸었던것을 삭제함
    그래서 pre_fix가 0000-00-00_에서 000000000000.0 형식으로 변경

--- pdf2png.py
+++ pdf2png.py
@@ -1,10 +1,11 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import sys
 import os
 import datetime
 import glob
 from wand.image import Image
+
 
 # 할일 : 폴더 내의 폴더(노트북 폴더)의 파일도 정상적으로 변경이 되는지 확인 필요
 
@@ -15,8 +16,9 @@ def ctime_prefix(name):
     :return: 파일 생성시간 prefix (str)
     """
     ctime = os.path.getmtime(name)
-    ctime = str(datetime.datetime.fromtimestamp(ctime))[:10] + "_"
+    ctime = str(ctime) + "_"
     return ctime
+
 
 def pdf2png(rename):
     """
@@ -31,38 +33,39 @@ def pdf2png(rename):
         return pngname
 
 
-def convert_pdf(pdflist):	
-	'''
+def convert_pdf(pdflist):
+    '''
 	각 폴더의 파일 리스트를 넘겨받아 pdf의 이름과 확장자를 변경
 	'''
-	ea = len(pdflist)
-	count = 0
-	for pdf in pdflist:
-		# prefix추가된 파일명
-		rename = os.path.join(os.path.dirname(pdf),ctime_prefix(pdf)+os.path.basename(pdf))
-		os.rename(pdf, rename)      # prefix추가된 파일명으로 변환
-		pngname = pdf2png(rename)   # pdf를 png로 변환
-		count += 1
-		print rename, "을 ",pngname,"으로 변환했습니다."
-		os.remove(rename)           # pdf 삭제
-		print rename,"을 삭제하였습니다"
-		print ea,"개중 ",count,"개가 완료되었습니다"
+    ea = len(pdflist)
+    count = 0
+    for pdf in pdflist:
+        # prefix추가된 파일명
+        rename = os.path.join(os.path.dirname(pdf), ctime_prefix(pdf) + os.path.basename(pdf))
+        os.rename(pdf, rename)  # prefix추가된 파일명으로 변환
+        pngname = pdf2png(rename)  # pdf를 png로 변환
+        count += 1
+        print rename, "을 ", pngname, "으로 변환했습니다."
+        os.remove(rename)  # pdf 삭제
+        print rename, "을 삭제하였습니다"
+        print ea, "개중 ", count, "개가 완료되었습니다"
+
 
 # 2.7한글 호환
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # 분류된 파일이 위치한 절대경로
-work_dir = "/Users/sinsky/Documents/Sync" 
-os.chdir(work_dir) 
+work_dir = "/Users/sinsky/Documents/Sync"
+os.chdir(work_dir)
 
 nbook_list = os.listdir(work_dir)
 
 # 각 폴더(노트북)의 pdf파일 이름과 확장자 변경
 for notebook in nbook_list:
-	notebook = os.path.join(work_dir,notebook)
-	pdflist = glob.glob("%s/*.pdf" % (notebook))  # 각 폴더의 pdf파일 추출
-	convert_pdf(pdflist)
+    notebook = os.path.join(work_dir, notebook)
+    pdflist = glob.glob("%s/*.pdf" % (notebook))  # 각 폴더의 pdf파일 추출
+    convert_pdf(pdflist)
 
 print "파일 변환이 완료되었습니다. 에버노트로 업로드를 시작합니다"
 import post2evernote
