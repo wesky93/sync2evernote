8643bc1b2b0ec6cce87a65ff84aca2fea463e958
wesky93 <wesky93@gmail.com>
2016-06-08 17:53:52 +0900

    폴더별 노트를 에버노트로 업로드 하는 코드 완성(테스트전)

--- post2evernote.py
+++ post2evernote.py
@@ -2,18 +2,20 @@
 from my_token import test_token, pro_token
 import sys
 import hashlib
+import glob
 import binascii
 import evernote.edam.type.ttypes as Types
-from os import listdir
+from os import listdir, path
 from evernote.api.client import EvernoteClient
 
 
+# --- 노트북 조회 및 확인 ---
 # 에버노트에 노트북 생성해주는 함수
 def mk_notebook(nbooks, note_store):
     for nbook in nbooks:
         notebook = Types.Notebook()
         notebook.name = nbook
-        cre_notebook = note_store.createNotebook(notebook)
+        note_store.createNotebook(notebook)
 
 
 # 에버노트의 노트북명과 guid로 딕셔너리를 만들어주는 함수
@@ -40,6 +42,53 @@ def match_notebooks(work_path, note_store):
     return bookguid(note_store)
 
 
+# --- 노트 생성,업로드 및 파일 삭제 ---
+# 파일을 받아 에버노트에 올릴 리소스 객체 생성
+def mk_resource(png):
+    with open(png, 'rb').read() as image:
+        md5 = hashlib.md5()
+        md5.update(image)
+        hash = md5.digest()
+        hash_hex = binascii.hexlify(hash)
+
+        data = Types.Data()
+        data.size = len(image)
+        data.bodyHash = hash
+        data.body = image
+
+        resource = Types.Resource()
+        resource.mime = 'image/png'
+        resource.data = data
+        return resource, hash_hex
+
+
+def file_info(png):
+    """
+	파일에서 생성 날짜와 파일명을 추출해준다.
+	:param png: 파일 절대경로
+	:return: 제목,생성날짜(추가예정)
+	"""
+    file = path.basename(png)
+    name = path.splitext(file)[0]
+    return name
+
+
+# 노트를 생성하여 에버노트에 업로드
+def post_note(note_store, png, book_guid):
+    resource, hash = mk_resource(png)
+    note = Types.Note()
+    note.title = file_info(png)
+    note.resources = [resource]
+    note.notebookGuid = book_guid
+    note.content = '<?xml version="1.0" encoding="UTF-8"?>'
+    note.content += '<!DOCTYPE en-note SYSTEM ' \
+                    '"http://xml.evernote.com/pub/enml2.dtd">'
+    note.content += '<en-note>Here is the Evernote logo:<br/>'
+    note.content += '<en-media type="image/png" hash="' + hash + '"/>'
+    note.content += '</en-note>'
+    note_store.createNotebook(note)
+
+
 # 한글 문제 해결용 코드
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -60,6 +109,14 @@ note_store = client.get_note_store()
 # 폴더명과 노트북명을 비교하여 존재유무 확인 및 생성
 notebooks = match_notebooks(work_path, note_store)
 
+# 폴더를 순회하며 노트 생성
+for notebook in listdir(work_path):
+    book_path = path.join(work_path, notebook)
+    if path.isdir(book_path):  # 디렉토리 인지 확인
+        book_guid = notebooks[notebook]  # 노트북의 guid값
+        file_list = glob.glob("%s/*.png" % (book_path))
+        for png in file_list:
+            post_note(note_store, png, book_guid)
 
 '''
 # 할일 : sync폴더에서 png파일만 추출
@@ -104,7 +161,7 @@ hash_hex = binascii.hexlify(hash)
 # at http://dev.evernote.com/documentation/cloud/chapters/ENML.php
 note.content = '<?xml version="1.0" encoding="UTF-8"?>'
 note.content += '<!DOCTYPE en-note SYSTEM ' \
-    '"http://xml.evernote.com/pub/enml2.dtd">'
+	'"http://xml.evernote.com/pub/enml2.dtd">'
 note.content += '<en-note>Here is the Evernote logo:<br/>'
 note.content += '<en-media type="image/png" hash="' + hash_hex + '"/>'
 note.content += '</en-note>'
