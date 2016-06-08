ed8d6b0f20e53c0f15ea2deaf19143dc9bdb8a89
wesky93 <wesky93@gmail.com>
2016-06-09 01:43:48 +0900

    폴더별로 순회하며  pdf을 변환하도록 변경
    현재 이전과 유사하게 png는 지원이 안된다느 오류로 파일변환이 불가능합니다.
    먼저 이전에 작성한 코드로 유사환경에서 테스트를 해보로 이미지매직 디벙깅을 해야할 수도 있음

--- pdf2png.py
+++ pdf2png.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os
-import datetime
+from os import path,listdir,rename,remove
 import glob
 from wand.image import Image
 
@@ -15,8 +14,8 @@ def ctime_prefix(name):
     :param path: 파일 절대경로
     :return: 파일 생성시간 prefix (str)
     """
-    ctime = os.path.getmtime(name)
-    ctime = str(ctime) + "_"
+    ctime = path.getmtime(name)
+    ctime = str(ctime)[:-2] + "_"
     return ctime
 
 
@@ -41,13 +40,16 @@ def convert_pdf(pdflist):
     count = 0
     for pdf in pdflist:
         # prefix추가된 파일명
-        rename = os.path.join(os.path.dirname(pdf), ctime_prefix(pdf) + os.path.basename(pdf))
-        os.rename(pdf, rename)  # prefix추가된 파일명으로 변환
-        pngname = pdf2png(rename)  # pdf를 png로 변환
+        re_name = path.join(path.dirname(pdf), ctime_prefix(pdf) + path.basename(pdf))
+        rename(pdf, re_name)  # prefix추가된 파일명으로 변환
+        pngname = pdf2png(re_name)  # pdf를 png로 변환
+        # wand 혹은 이미지 매직에서 png를 지원하지 않는 오류가 계속 발생
+        # imagemagick을 재설치 해도 마찬가지 고스트스크립트를 써도 마찬가지이나 정작 이미지 매직으로 pdf가 png로 바뀌는것이 문제 없음 확임
+        # 필요하다면 이미지 매직을 디버깅(?!?) 해야할 수도 ....
         count += 1
-        print rename, "을 ", pngname, "으로 변환했습니다."
-        os.remove(rename)  # pdf 삭제
-        print rename, "을 삭제하였습니다"
+        print re_name, "을 ", pngname, "으로 변환했습니다."
+        remove(re_name)  # pdf 삭제
+        print re_name, "을 삭제하였습니다"
         print ea, "개중 ", count, "개가 완료되었습니다"
 
 
@@ -56,16 +58,15 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 # 분류된 파일이 위치한 절대경로
-work_dir = "/Users/sinsky/Documents/Sync"
-os.chdir(work_dir)
-
-nbook_list = os.listdir(work_dir)
+work_dir = "/Users/sinsky/Desktop/sync 노트정리/에버노트"
 
 # 각 폴더(노트북)의 pdf파일 이름과 확장자 변경
-for notebook in nbook_list:
-    notebook = os.path.join(work_dir, notebook)
-    pdflist = glob.glob("%s/*.pdf" % (notebook))  # 각 폴더의 pdf파일 추출
-    convert_pdf(pdflist)
+for notebook in listdir(work_dir):
+    book_path = path.join(work_dir, notebook)
+    if path.isdir(book_path):  # 디렉토리 인지 확인
+        file_list = glob.glob("%s/*.pdf" % (book_path))
+        convert_pdf(file_list)
+
 
 print "파일 변환이 완료되었습니다. 에버노트로 업로드를 시작합니다"
-import post2evernote
+#import post2evernote
